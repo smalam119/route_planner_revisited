d5ba245b78632feec4584934c97b9051737a7cec
smalam119 <smalam119@gmail.com>
2017-11-30 00:32:26 +0600

    action buttons added and styled

--- app/src/main/java/com/thyme/smalam119/routeplannerapplication/Map/MainActivity.java
+++ app/src/main/java/com/thyme/smalam119/routeplannerapplication/Map/MainActivity.java
@@ -2,12 +2,18 @@ package com.thyme.smalam119.routeplannerapplication.Map;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.github.clans.fab.FloatingActionButton;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.thyme.smalam119.routeplannerapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
     private RPAOnMapReadyCallback mOnMapReadyCallback;
+    private FloatingActionButton mProfileActionButton;
+    private FloatingActionButton mNotificationActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +23,24 @@ public class MainActivity extends AppCompatActivity {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(mOnMapReadyCallback);
+        prepareView();
+    }
+
+    private void prepareView() {
+        mProfileActionButton = (FloatingActionButton) findViewById(R.id.profile_action_button);
+        mProfileActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("onClick", "Profile Pressed");
+            }
+        });
+
+        mNotificationActionButton = (FloatingActionButton) findViewById(R.id.notification_action_button);
+        mNotificationActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("onClick", "Notification Pressed");
+            }
+        });
     }
 }
